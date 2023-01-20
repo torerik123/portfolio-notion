ffb81007da2178487ac3251844af5fa43ea5fe55
torerik123 <62653782+torerik123@users.noreply.github.com>
2023-01-20 17:20:11 +0100

    deploy to pages

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
-</template>
+	<NotionRenderer v-if="data" :blockMap="data" fullPage />
+  </template>  
+
+<script setup lang="ts">
+	import { NotionRenderer, getPageBlocks, useGetPageBlocks } from "vue3-notion"
+	import { ref, onMounted } from "vue"
+
+	const data = ref()
+
+	onMounted(async () => {
+		data.value = await getPageBlocks("17e24124dc4b41a5a489c15f70eafed7")
+	})
+
+</script>
+
+<style>
+@import "vue3-notion/dist/style.css"; /* optional Notion-like styles */
+@import "prismjs/themes/prism.css";
+@import "katex/dist/katex.min.css";
+
+html {
+	margin: 0;
+	background-color: black;
+}
+
+.notion-text {
+	color: var(--notion-gray);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [
+        ["vue3-notion/nuxt", { css: true }], // css is not imported by default. Set `true` to import css
+    ],
 })
